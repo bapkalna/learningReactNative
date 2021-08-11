796336c4312b19635b85bf1b3acc27743d1692b6
Baiba Apkalna <apkalna@gmail.com>
2021-08-11 12:34:50 +0300

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -6,11 +6,12 @@ import {
   StyleSheet,
   Switch,
   Button,
-  Alert
+  Alert,
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { Picker } from "@react-native-community/picker";
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 class Reservation extends Component {
   constructor(props) {
@@ -32,21 +33,28 @@ class Reservation extends Component {
     console.log(JSON.stringify(this.state));
 
     Alert.alert(
-        "Begin Search?",
-        `Number of Campers: ${this.state.campers} \nHike In? ${this.state.hikeIn} \nDate: ${this.state.date}`,
-        [
-          {
-            text: "Cancel",
-            style: "cancel",
-            onPress: () => console.log("Cancel Pressed"),
+      "Begin Search?",
+      `Number of Campers: ${this.state.campers} \nHike In? ${this.state.hikeIn} \nDate: ${this.state.date}`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+          onPress: () => {
+            console.log("Cancel Pressed"), this.resetForm();
           },
-          {
-            text: "OK",
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            this.presentLocalNotification(
+              this.state.date.toLocaleDateString("en-US")
+            );
+            this.resetForm();
           },
-        ],
-        { cancelable: false }
-      );
-
+        },
+      ],
+      { cancelable: false }
+    );
   }
 
   resetForm() {
@@ -58,10 +66,33 @@ class Reservation extends Component {
     });
   }
 
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${date} requested`,
+        },
+        trigger: null,
+      });
+    }
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
+  }
+
   render() {
-
-    
-
     return (
       <ScrollView>
         <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
@@ -143,7 +174,6 @@ const styles = StyleSheet.create({
   formItem: {
     flex: 1,
   },
-  
 });
 
 export default Reservation;
